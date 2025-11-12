aec12b8e3ccc826505233844c361bdfbcc007837
remoteyj-alt <remoteyj@gmail.com>
2025-11-12 13:29:11 +0900

    Update oracle_connect.py

--- oracle_connect.py
+++ oracle_connect.py
@@ -1,6 +1,6 @@
 import oracledb
 
-user, password, host, port, service_name = "test", "test", "10.0.7.5", 1521, "kgcapp"
+user, password, host, port, service_name = "tph", "yplgcc9y", "10.0.7.5", 1521, "kgcapp"
 dsn = f"{host}:{port}/{service_name}"
 
 try:
@@ -8,17 +8,17 @@ try:
     cursor = conn.cursor()
 
     cursor.execute("SELECT table_name FROM user_tables")
-    print("테이블 목록:")
+    print("Table List:")
     for t in cursor.fetchall():
         print(t[0])
 
     cursor.execute("SELECT * FROM your_table_name WHERE ROWNUM <= 5")
-    print("\n샘플 데이터:")
+    print("\nData:")
     for row in cursor.fetchall():
         print(row)
 
 except oracledb.Error as e:
-    print("오류 발생:", e)
+    print("Error:", e)
 
 finally:
     if 'conn' in locals():
