1149e8ccae640e3129ecbd054e3db0fe71db1f02
remoteyj-alt <remoteyj@gmail.com>
2025-11-13 21:15:46 +0900

    Update JdbcQuery.java

--- JdbcQuery.java
+++ JdbcQuery.java
@@ -4,7 +4,7 @@ public class JdbcQuery {
     public static void main(String[] args) throws Exception {
         Class.forName("oracle.jdbc.driver.OracleDriver");
         Connection conn = DriverManager.getConnection(
-            "jdbc:oracle:thin:@210.100.7.65:1521/kgcapp", "tph", "yplgcc9y");
+            "jdbc:oracle:thin:@1.1.1.1:1521/123", "123", "123");
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT SYSDATE FROM DUAL");
         while (rs.next()) {
